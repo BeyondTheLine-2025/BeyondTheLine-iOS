b604b01aaaaddc7c4040fc53bcd1c9ceee670f42
mini-min <mini@miniui-MacBookAir.local>
2025-06-16 22:56:37 +0900

    [Feat] #3 - Implementation Sheet

--- BeyondTheLine-iOS/BeyondTheLine-iOS/Core/Modifiers/BeyondTheLineBottomSheetModifier.swift
+++ BeyondTheLine-iOS/BeyondTheLine-iOS/Core/Modifiers/BeyondTheLineBottomSheetModifier.swift
@@ -5,4 +5,52 @@
 //  Created by mini on 6/14/25.
 //
 
-import Foundation
+import SwiftUI
+
+struct BeyondTheLineBottomSheetModifier: ViewModifier {
+    @Binding var isPresented: Bool
+    let sheetType: BeyondTheLineBottomSheetType
+    let description: String
+    let buttonTitle: String
+    let onTapButton: () -> Void
+
+    func body(content: Content) -> some View {
+        ZStack {
+            content
+            
+            if isPresented {
+                BeyondTheLineBottomSheet(
+                    sheetType: sheetType,
+                    desctiption: description,
+                    buttonTitle: buttonTitle
+                ) {
+                    withAnimation {
+                        onTapButton()
+                        isPresented = false
+                    }
+                }
+                .transition(.move(edge: .bottom))
+            }
+        }
+    }
+}
+
+extension View {
+    func bottomSheet(
+        isPresented: Binding<Bool>,
+        sheetType: BeyondTheLineBottomSheetType,
+        description: String,
+        buttonTitle: String,
+        onTapButton: @escaping () -> Void
+    ) -> some View {
+        self.modifier(
+            BeyondTheLineBottomSheetModifier(
+                isPresented: isPresented,
+                sheetType: sheetType,
+                description: description,
+                buttonTitle: buttonTitle,
+                onTapButton: onTapButton
+            )
+        )
+    }
+}
